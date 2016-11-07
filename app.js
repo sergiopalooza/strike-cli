896a5ea3cfe73e923a776d634298d781c12c28ee
Sergio Flores <sergio@appiphony.com>
2016-11-07 15:09:17 -0600

    Created and cleaned up function for bundle and other definition creation

--- app.js
+++ app.js
@@ -7,13 +7,63 @@ var password = process.env.SF_STRIKE_PASSWORD;
 
 conn.login(username, password, function(err, res) {
 	if (err) { return console.error(err); }
-	conn.query('SELECT Id, Name FROM Account', function(err, res) {
-    	if (err) { return console.error(err); }
-    	console.log(res);
-	});
+	var componentName = createComponentName();
+	createAuraDefinitionBundle(componentName);
 });
 
-createComponentName();
+function createAuraDefinitionBundle(componentName){
+	conn.tooling.sobject('AuraDefinitionBundle').create({
+		Description: 'I was created from Strike-CLI', // my description
+	  	DeveloperName: componentName,
+	  	MasterLabel: componentName, 
+	  	ApiVersion:'32.0'
+	}, 	function(err, res){
+		if (err) { return console.error(err); }
+		console.log(res);
+
+		var bundleId = res.id;
+
+		createComponentApplication(bundleId);
+		createComponentController(bundleId);
+		createComponentHelper(bundleId);
+	});
+}
+
+function createComponentApplication(bundleId){
+	conn.tooling.sobject('AuraDefinition').create({
+		AuraDefinitionBundleId: bundleId,
+	    DefType: 'APPLICATION',
+	    Format: 'XML',
+	    Source: '<aura:application></aura:application>'
+	  }, function(err, res) {
+	  if (err) { return console.error(err); }
+	  console.log(res);
+	});
+}
+
+function createComponentController(bundleId){
+	conn.tooling.sobject('AuraDefinition').create({
+		AuraDefinitionBundleId: bundleId,
+	    DefType: 'CONTROLLER',
+	    Format: 'JS',
+	    Source: '({\n\tmyAction : function(component, event, helper) {\n\t}\n})'
+	  }, function(err, res) {
+	  if (err) { return console.error(err); }
+	  console.log(res);
+	});
+}
+
+function createComponentHelper(bundleId){
+	conn.tooling.sobject('AuraDefinition').create({
+	AuraDefinitionBundleId: bundleId,
+	    DefType: 'HELPER',
+	    Format: 'JS',
+	    Source: '({\n\thelperMethod : function() {\n\t}\n})'
+	  }, function(err, res) {
+	  if (err) { return console.error(err); }
+	  console.log(res);
+	});
+}
 
 function createComponentName(){
 	var date = new Date();
@@ -25,5 +75,5 @@ function createComponentName(){
 	var randomInt = dateComponents.join("");
 	var componentName = 'Prototype_Component' + randomInt;
 	console.log(componentName);
-
+	return componentName;
 }
